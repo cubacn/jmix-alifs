5d9ebd81691d81daa925e8a672b384012c8dfac0
Ray.Lv <Ray.Lv@jmix.cn>
2022-02-24 12:40:07 +0800

    完善构建和测试过程

--- alifs/src/test/groovy/AliFileStorageTest.groovy
+++ alifs/src/test/groovy/AliFileStorageTest.groovy
@@ -4,6 +4,7 @@ import io.jmix.core.CoreConfiguration
 import io.jmix.core.FileRef
 import io.jmix.core.FileStorage
 import io.jmix.core.UuidProvider
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
@@ -24,40 +25,20 @@ class AliFileStorageTest extends Specification {
     private FileStorage fileStorage
 
 
-    def "save stream"(){
+    def "save open remove"(){
         def fileName=UuidProvider.createUuid().toString()+".txt";
-        def fileStream=this.getClass().getClassLoader().getResourceAsStream("files/simple.txt");
-        def fileRef=fileStorage.saveStream(fileName,fileStream);
+        String string = "Text for testing.";
+        InputStream inputStream = new ByteArrayInputStream(string.getBytes());
+        def fileRef=fileStorage.saveStream(fileName,inputStream);
+         def fileExists= fileStorage.fileExists(fileRef)
         def openedStream=fileStorage.openStream(fileRef);
+        def fileOpened =openedStream!=null
+        fileStorage.removeFile(fileRef)
         expect:
-            openedStream!=null
-    }
-
-    def "fileExists"() {
-        def storageName = fileStorage.getStorageName()
-        def fileKey = "2021/11/05/38a0405a-c885-5f75-cef7-6727b71838c1.txt"
-        def fileName="38a0405a-c885-5f75-cef7-6727b71838c1.txt"
-
-        def fileref = new FileRef(storageName, fileKey, fileName);
-        def exists = fileStorage.fileExists(fileref)
-
-        expect:  exists
-
-    }
-
-
-    def "removeFile"(){
-        def storageName = fileStorage.getStorageName()
-        def fileKey = "2021/11/05/38a0405a-c885-5f75-cef7-6727b71838c1.txt"
-        def fileName="38a0405a-c885-5f75-cef7-6727b71838c1.txt"
-
-        def fileref = new FileRef(storageName, fileKey, fileName);
-        fileStorage.removeFile(fileref)
-
-
-        def exists = fileStorage.fileExists(fileref)
-
-        expect:  !exists
+        verifyAll {
+            fileExists
+            fileOpened
+        }
     }
 
     def "ali storage initialized"() {
